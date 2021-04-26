0768f5fb5fab7b447bc1d19cf1fbd8ed594eccb7
Jamt0 <jamt042831@gmail.com>
2021-04-25 23:07:58 -0300

    first commit

---
+++ src/pages/Tab2.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import {
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonMenuButton,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import "./Tab2.css";
+
+const Tab2: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonMenuButton />
+          </IonButtons>
+          <IonTitle>Tab 2</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen></IonContent>
+    </IonPage>
+  );
+};
+
+export default Tab2;
---
+++ src/pages/Tab3.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import {
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonMenuButton,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import "./Tab3.css";
+
+const Tab3: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonMenuButton />
+          </IonButtons>
+          <IonTitle>Tab 3</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen></IonContent>
+    </IonPage>
+  );
+};
+
+export default Tab3;
